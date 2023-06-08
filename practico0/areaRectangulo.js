33aab9caf3ee7bcea5fc0a84591b1c7e10356831
mariano <cirilliluis@gmail.com>
2023-06-08 19:14:42 -0300

    practico0

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,9 @@
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area); //resultado ejercicio 2.a//
+
+base = 12.5;
+altura = 5.3;
+area = base * altura;
+console.log(area); // resultado ejercicio 2.b //
---
+++ practico0/multiplicacion.js
@@ -0,0 +1,4 @@
+let num1 = 2;
+let num2 = 3;
+let num3 = 4;
+console.log(num1 * num2 * num3); //resultado 1.b//
